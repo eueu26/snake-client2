87b1cc31af2641b164444183255712872ba735e4
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-04-23 23:54:48 -0400

    refactored input and aquired it in play

---
+++ input.js
@@ -0,0 +1,31 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  const handleUserInput = function() {
+    
+    stdin.on("data", (data) => {
+      // console.log("data clicked: ", data);
+      if (data === "\u0003") {
+        process.exit();
+      }
+      if (data === 'w') {
+        stdin.write("Move: up\n");
+      }
+      if (data === 'a') {
+        stdin.write("Move: left\n");
+      }
+      if (data === 's') {
+        stdin.write("Move: down\n");
+      }
+      if (data === 'd') {
+        stdin.write("Move: right\n");
+      }
+    });
+  };
+  return stdin.on("data", handleUserInput);
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,36 +1,8 @@
-const connect = require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
+setupInput();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  
-  const handleUserInput = function() {
-    
-    stdin.on("data", (data) => {
-      // console.log("data clicked: ", data);
-      if (data === "\u0003") {
-        process.exit();
-      }
-      if (data === 'w') {
-        stdin.write("Move: up\n");
-      }
-      if (data === 'a') {
-        stdin.write("Move: left\n");
-      }
-      if (data === 's') {
-        stdin.write("Move: down\n");
-      }
-      if (data === 'd') {
-        stdin.write("Move: right\n");
-      }
-    });
-  };
-  return stdin.on("data", handleUserInput);
-};
