24b892e744cc3c245ce6282f8ed0738456e8b9b6
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-04-23 23:29:37 -0400

    added name and connect statement

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "10.0.0.24", // IP address here,
+    port: 50541, // PORT number here,
+  });
+  
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.write("Name: EFG");
+  return conn;
+};
+
+module.exports = connect;
